01fc5e6595f86bcb0b2db9b823b49202673fc9e5
Akhila499 <akhilathota499@gmail.com>
2021-11-02 14:34:26 -0400

    small changes

---
+++ client/src/api/useDataFromApi.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const useDataFromApi = () => {
+  const [appState, setAppState] = useState({
+    wheatherData: [],
+    isFetching:false
+  });
+  
+  useEffect(() => {
+    //get data from axios api
+    console.log('testing env', process.env.REACT_APP_WEATHER_API_KEY)
+    const fetchData = async () => {
+      try{
+        setAppState({wheatherData:appState.wheatherData, isFetching: true});
+        axios
+          .get(`https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
+          .then((res)=>{
+            setAppState({wheatherData: res.data, isFetching: false})
+            console.log('res',res, res.data)
+          })
+      }catch(exception){
+        console.log(exception);
+        setAppState({wheatherData:appState.wheatherData, isFetching:false});
+      }
+    };
+    fetchData();
+    
+  }, [])
+
+
+  return {
+    appState
+  }
+}
+
+export default useDataFromApi
